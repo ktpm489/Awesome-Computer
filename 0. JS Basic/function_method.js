264c207a803a7dbb9d2e8677fb7163661cc7bad1
WonJunKim <kimjnsjwj@gmail.com>
2018-03-19 23:48:17 +0900

    promise summary

--- 0. JS Basic/function_method.js
+++ 0. JS Basic/function_method.js
@@ -1,1 +1,11 @@
 //함수도 객체이므로, 함수에도 메소드가 있다. 대표적으로 call, apply, bind가 있다
+
+'use strict'
+
+function plus(a, b, c) {
+    return a + b + c;
+}
+
+console.log(plus(1, 2, 3));
+console.log(plus.apply(null, [1, 2, 3]));
+console.log(plus.call(null, 1, 2, 3));
--- 0. JS Basic/this.js
+++ 0. JS Basic/this.js
@@ -1,23 +1,21 @@
 //참고 : https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/this
 //참고2 : https://stackoverflow.com/questions/34838659/the-this-keyword-behaves-differently-in-nodejs-and-browser
+//참고3 : http://poiemaweb.com/js-this
+//참고4 : http://webframeworks.kr/tutorials/translate/explanation-of-this-in-javascript-1/
 
 var obj1;
 var obj2;
 
 function x() {
     obj1 = this;
-}
-
-function y() {
     obj2 = this;
 }
 
-x();
+function y() {
+    this.x();
+}
+
 y();
 
 console.log(obj1 === obj2);     //크롬과 Node.js에서 둘다 true
 console.log(obj1 === this);      //크롬에서는 true, Node.js에서는 false
-
-/*
-    좀 더 알아봐야되요
-*/
