2c21c5ea7ef84a408cb533f60c25db724bc099ff
WonJunKim <kimjnsjwj@gmail.com>
2018-03-21 23:54:36 +0900

    function declare

---
+++ 0. JS Basic/function.js
@@ -0,0 +1,15 @@
+//함수를 정의할 때에는 여러가지 방식을 사용할 수 있다.
+
+function add(x, y) {
+    return x + y;
+}
+
+var add2 = function (x, y) {
+    return x + y;
+}
+
+var add3 = (x, y) => {
+    return x + y;
+}
+
+var add4 = (x, y) => x + y;
