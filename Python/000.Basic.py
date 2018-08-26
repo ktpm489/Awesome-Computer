12030a5932112e9f0520b3ce8a58cc7c1d3b1fc4
WonJunKim <kimjnsjwj@gmail.com>
2018-08-26 14:45:39 +0900

    add Python

---
+++ Python/000.Basic.py
@@ -0,0 +1,14 @@
+# Python 다양한 표준 라이브러리를 import문을 통해 사용할 수 있다
+import math
+
+print(math.sqrt(81))
+
+# 코멘트는 #을 이용한다
+print('Hello, Pyhotn!')
+
+# 블록을 표시하기 위해 중괄호가 아닌 들여쓰기를 사용한다
+x = 10
+if x > 0:
+    print(x, 'is bigger than 0!')
+else:
+    print(x, 'is smaller than 0!')
